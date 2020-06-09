1ee2ab1e59eb567ad3789beebe7da0cdaed940bc
Ricardo Lima Mazzolli <ricarm@gmail.com>
2020-06-09 14:08:36 -0300

    Aula 01

---
+++ Aulas/aula01.py
@@ -0,0 +1,11 @@
+#coding: utf-8
+
+# Este é o meu primeiro programa em Python!
+
+# 1. Entrada de dados
+# 2. Processamento
+# 3. Saída de dados
+
+a = input('Digite o valor de A: ')
+resultado = int(a) + 5
+print('O número foi somado a 5 e o resultado foi', resultado)
