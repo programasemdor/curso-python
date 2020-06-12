f19a0799dcff1ca5c68e329f226f1287c2eb7aeb
Ricardo Lima Mazzolli <ricarm@gmail.com>
2020-06-11 22:16:56 -0300

    Exercícios Aula 01

---
+++ Exercícios/Aula 01/exercicio01_2.py
@@ -0,0 +1,15 @@
+#coding: utf-8
+
+#Crie um sistema em Python que mostra a soma e a média aritmética de dois números informados pelo usuário.
+
+#Entrada de dados
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+#Processamento
+soma = n1 + n2
+media = soma / 2
+
+#Saída de Dados
+print('Soma:', soma)
+print('Média:', media)
